914b4f875cf542480044eb76325f44a95cf0cc7d
Fan Zhang <zhafang@google.com>
2017-09-26 14:24:05 -0700

    Fixed duplicated primitives

--- make_compressed_model_and_copy_to_gltftest.sh
+++ make_compressed_model_and_copy_to_gltftest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#modelNames=(Box Duck 2CylinderEngine ReciprocatingSaw GearboxAssy Buggy CesiumMan RiggedSimple RiggedFigure Monster VC BrainStem)
-modelNames=(RiggedSimple RiggedFigure BrainStem CesiumMan)
+modelNames=(Box Duck 2CylinderEngine ReciprocatingSaw GearboxAssy Buggy CesiumMan RiggedSimple RiggedFigure Monster VC BrainStem)
+#modelNames=(RiggedSimple RiggedFigure BrainStem CesiumMan)
+#modelNames=(GearboxAssy)
 for modelName in ${modelNames[@]}; do
   echo $modelName
   rm -rf output/* && node ./bin/gltf-pipeline.js -i ~/glTF-Sample-Models/2.0/$modelName/glTF/$modelName.gltf -d -s -o $modelName.gltf && rm -rf ~/gltf-test/sampleModels/$modelName/glTF-Draco/* && cp -r output/* ~/gltf-test/sampleModels/$modelName/glTF-Draco/
