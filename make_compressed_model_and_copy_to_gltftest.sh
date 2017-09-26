f49df7a0b37c8801c8b429c9f73fc0cc15c56979
Fan Zhang <zhafang@google.com>
2017-09-26 13:19:18 -0700

    Generic attribute quantization to 12

--- make_compressed_model_and_copy_to_gltftest.sh
+++ make_compressed_model_and_copy_to_gltftest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-modelNames=(Box Duck 2CylinderEngine ReciprocatingSaw GearboxAssy Buggy CesiumMan RiggedSimple RiggedFigure Monster VC BrainStem)
-#modelNames=(RiggedSimple)
+#modelNames=(Box Duck 2CylinderEngine ReciprocatingSaw GearboxAssy Buggy CesiumMan RiggedSimple RiggedFigure Monster VC BrainStem)
+modelNames=(RiggedSimple RiggedFigure BrainStem CesiumMan)
 for modelName in ${modelNames[@]}; do
   echo $modelName
   rm -rf output/* && node ./bin/gltf-pipeline.js -i ~/glTF-Sample-Models/2.0/$modelName/glTF/$modelName.gltf -d -s -o $modelName.gltf && rm -rf ~/gltf-test/sampleModels/$modelName/glTF-Draco/* && cp -r output/* ~/gltf-test/sampleModels/$modelName/glTF-Draco/
