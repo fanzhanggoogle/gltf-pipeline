4a14588f0b241c743055c4e3044a323447577ad2
Fan Zhang <zhafang@google.com>
2017-09-26 12:27:54 -0700

    works now

--- make_compressed_model_and_copy_to_gltftest.sh
+++ make_compressed_model_and_copy_to_gltftest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-modelNames=(Box Duck 2CylinderEngine ReciprocatingSaw GearboxAssy Buggy CesiumMan RiggedSimple)
+modelNames=(Box Duck 2CylinderEngine ReciprocatingSaw GearboxAssy Buggy CesiumMan RiggedSimple RiggedFigure Monster VC BrainStem)
 #modelNames=(RiggedSimple)
 for modelName in ${modelNames[@]}; do
   echo $modelName
