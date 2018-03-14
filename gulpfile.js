8f7a3f6de6065144227a237edff145de62b5270f
Frank Galligan <frankgalligan@gmail.com>
2018-03-14 15:15:44 -0700

    Merge 2.0 branch from head fork to base fork

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,8 @@ var Cesium = require('cesium');
 var child_process = require('child_process');
 var fsExtra = require('fs-extra');
 var gulp = require('gulp');
-var gulpJshint = require('gulp-jshint');
 var Jasmine = require('jasmine');
-var JasmineSpecReporter = require('jasmine-spec-reporter');
+var jasmineSpecReporter = require('jasmine-spec-reporter');
 var open = require('open');
 var path = require('path');
 var Promise = require('bluebird');
@@ -16,33 +15,13 @@ var defined = Cesium.defined;
 var DeveloperError = Cesium.DeveloperError;
 var argv = yargs.argv;
 
-var fsExtraReadFile = Promise.promisify(fsExtra.readFile);
-var fsExtraOutputFile = Promise.promisify(fsExtra.outputFile);
-
 // Add third-party node module binaries to the system path
 // since some tasks need to call them directly.
 var environmentSeparator = process.platform === 'win32' ? ';' : ':';
 var nodeBinaries = path.join(__dirname, 'node_modules', '.bin');
 process.env.PATH += environmentSeparator + nodeBinaries;
 
-var jsHintFiles = ['**/*.js', '!node_modules/**', '!coverage/**', '!doc/**', '!dist/**'];
 var specFiles = ['**/*.js', '!node_modules/**', '!coverage/**', '!dist/**'];
-
-gulp.task('jsHint', function () {
-    var stream = gulp.src(jsHintFiles)
-        .pipe(gulpJshint())
-        .pipe(gulpJshint.reporter('jshint-stylish'));
-
-    if (argv.failTaskOnError) {
-        stream = stream.pipe(gulpJshint.reporter('fail'));
-    }
-
-    return stream;
-});
-
-gulp.task('jsHint-watch', function () {
-    gulp.watch(jsHintFiles, ['jsHint']);
-});
 
 function excludeCompressedTextures(jasmine) {
     var excludedSpecs = ['compressTexturesSpec.js', 'compressTexturesMultipleFormatsSpec.js'];
@@ -67,7 +46,7 @@ gulp.task('test', function (done) {
         // Travis runs Ubuntu 12.04.5 which has glibc 2.15, while crunch requires glibc 2.22 or higher
         excludeCompressedTextures(jasmine);
     }
-    jasmine.addReporter(new JasmineSpecReporter({
+    jasmine.addReporter(new jasmineSpecReporter.SpecReporter({
         displaySuccessfulSpec: !defined(argv.suppressPassed) || !argv.suppressPassed
     }));
     jasmine.execute();
@@ -101,9 +80,9 @@ gulp.task('coverage', function () {
         additionalExcludes += '-x "specs/lib/compressTexturesMultipleFormatsSpec.js"';
     }
 
-    child_process.execSync('istanbul' +
-        ' cover' +
-        ' --include-all-sources' +
+    child_process.execSync('nyc' +
+        ' --all' +
+        ' --reporter=lcov' +
         ' --dir coverage' +
         ' -x "specs/**" -x "bin/**" -x "coverage/**" -x "dist/**" -x "index.js" -x "gulpfile.js"' + additionalExcludes +
         ' node_modules/jasmine/bin/jasmine.js' +
@@ -194,12 +173,11 @@ function amdify(source, subDependencyMapping) {
             paths.push(requireMapping[variable]);
         }
     }
-    var defineHeader = '/*global define*/\n' +
-            'define([], function() {\n    ';
+    var defineHeader = 'define([], function() {\n    ';
     if (paths.length > 0) {
         var definePathsHeader = '\'' + paths.join('\',\n        \'') + '\'';
         var defineVariablesHeader = variables.join(',\n        ');
-        defineHeader = '/*global define*/\n' +
+        defineHeader =
             'define([\n' +
             '        ' + definePathsHeader + '\n' +
             '    ], function(\n' +
@@ -260,8 +238,7 @@ function combine(source) {
             paths.push(requireMapping[variable]);
         }
     }
-    var defineHeader = '/*global define*/\n' +
-        'var ' + returnValue + ' = (function() {\n    ';
+    var defineHeader = 'var ' + returnValue + ' = (function() {\n    ';
     var defineFooter = '\n}());\n';
     if (defined(returnValue)) {
         defineFooter = '\n    return ' + returnValue + ';' + defineFooter;
@@ -286,17 +263,16 @@ gulp.task('build-cesium', function () {
         'ForEach.js',
         'getAccessorByteStride.js',
         'getJointCountForMaterials.js',
-        'glslTypeToWebGLConstant.js',
+        'getStatistics.js',
         'numberOfComponentsForType.js',
         'parseBinaryGltf.js',
+        'pbrToMaterialsCommon.js',
         'processModelMaterialsCommon.js',
-        'processPbrMetallicRoughness.js',
         'removePipelineExtras.js',
         'removeExtensionsRequired.js',
         'removeExtensionsUsed.js',
         'techniqueParameterForSemantic.js',
-        'updateVersion.js',
-        'webGLConstantToGlslType.js'
+        'updateVersion.js'
     ];
     var subDependencyMapping = {
         cesium : {
@@ -305,12 +281,12 @@ gulp.task('build-cesium', function () {
     };
     Promise.map(files, function(fileName) {
         var filePath = path.join(basePath, fileName);
-        return fsExtraReadFile(filePath)
+        return fsExtra.readFile(filePath)
             .then(function(buffer) {
                 var source = buffer.toString();
                 source = amdify(source, subDependencyMapping);
                 var outputPath = path.join(outputDir, fileName);
-                return fsExtraOutputFile(outputPath, source);
+                return fsExtra.outputFile(outputPath, source);
             });
     });
 });
@@ -323,12 +299,12 @@ gulp.task('build-cesium-combine', function () {
     ];
     Promise.map(files, function(fileName) {
         var filePath = path.join(basePath, fileName);
-        return fsExtraReadFile(filePath)
+        return fsExtra.readFile(filePath)
             .then(function(buffer) {
                 var source = buffer.toString();
                 source = combine(source);
                 var outputPath = path.join(outputDir, fileName);
-                return fsExtraOutputFile(outputPath, source);
+                return fsExtra.outputFile(outputPath, source);
             });
     });
 });
